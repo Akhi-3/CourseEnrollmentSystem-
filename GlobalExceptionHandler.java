291a5e1f4f4648d041a53946ff3dd65b69f50390
Akhilandeshwari <133636934+Akhi-3@users.noreply.github.com>
2025-07-10 20:19:15 +0530

    Create GlobalExceptionHandler.java

---
+++ GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package jsp.courseenrollmentsystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import jsp.courseenrollmentsystem.dto.ResponseStructure;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(IdNotFoundException.class)
+	public ResponseEntity<ResponseStructure<String>> handleIdNotFoundException(IdNotFoundException exception) {
+		ResponseStructure<String> structure = new ResponseStructure<String>();
+		
+		structure.setStatusCode(HttpStatus.NOT_FOUND.value());
+		structure.setMessage("Failure");
+		structure.setData(exception.getMessage());
+		
+		return new ResponseEntity<ResponseStructure<String>>(structure, HttpStatus.NOT_FOUND); 
+	}
+}
